02bbcf9a5796dc019701587d7b760152bc7e07a8
Tiago Ribolli <ribollitiago@gmail.com>
2025-01-15 06:19:37 -0800

    Add Design Login: - Eye logic in input password

--- src/app/components/primary-input/primary-input.component.ts
+++ src/app/components/primary-input/primary-input.component.ts
@@ -1,34 +1,45 @@
-import { Component, forwardRef, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
-type InputTypes = "text" | "email" | "password"
+
+type InputTypes = "text" | "email" | "password";
 
 @Component({
   selector: 'app-primary-input',
+  standalone: true,
   imports: [
+    CommonModule,
     ReactiveFormsModule
   ],
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => PrimaryInputComponent),
     multi: true
-  }
-  ],
+  }],
   templateUrl: './primary-input.component.html',
-  styleUrl: './primary-input.component.scss'
+  styleUrls: ['./primary-input.component.scss']
 })
-export class PrimaryInputComponent implements ControlValueAccessor {
+export class PrimaryInputComponent implements ControlValueAccessor, OnInit {
   @Input() type: InputTypes = "text";
   @Input() placeholder: string = "";
   @Input() label: string = "";
   @Input() inputName: string = "";
+  @Input() isPassword: boolean = false;
+
+  fieldTextType?: boolean;
 
   value: string = '';
   onChange: any = () => { }
   onTouched: any = () => { }
 
+  ngOnInit() {
+    // Inicializa o tipo do campo com base em isPassword
+    this.fieldTextType = !this.isPassword;
+  }
+
   onInput(event: Event) {
-    const value = (event.target as HTMLInputElement).value
-    this.onChange(value)
+    const value = (event.target as HTMLInputElement).value;
+    this.onChange(value);
   }
 
   writeValue(value: any): void {
@@ -44,4 +55,8 @@ export class PrimaryInputComponent implements ControlValueAccessor {
   }
 
   setDisabledState(isDisabled: boolean): void { }
+
+  toggleFieldTextType() {
+    this.fieldTextType = !this.fieldTextType;
+  }
 }
