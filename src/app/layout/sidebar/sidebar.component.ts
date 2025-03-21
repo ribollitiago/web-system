8ac87a886b72d719a3442e6af216f41cb3b9461c
Tiago Ribolli <ribollitiago@gmail.com>
2025-03-21 10:32:53 -0300

    Sidebar - Bug Fix in the opening and closing animations of the sidenav-link submenu - Organization of SVG icons in folders

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output, Input, ViewChild } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, EventEmitter, Output, Input, ViewChild, OnDestroy, OnInit } from '@angular/core';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { TranslationService } from '../../services/translate.service';
+import { filter, Subscription } from 'rxjs';
 
 interface SidebarItem {
   routeLink?: string;
@@ -21,8 +22,7 @@ interface SidebarItem {
   styleUrl: './sidebar.component.scss',
   providers: [TranslationService]
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements OnInit, OnDestroy {
   @Input() isLeftSidebarCollapsed: boolean = false;
   @Output() changeIsLeftSidebarCollapsed = new EventEmitter<boolean>();
   @ViewChild(SearchInputComponent) searchInputComponent!: SearchInputComponent;
@@ -35,20 +35,74 @@ export class SidebarComponent {
   linkAnalytics: string = '';
 
   openedSubmenuIndex: number | null = null;
+  activeParentIndex: number | null = null;
+  private routerSubscription: Subscription;
+  private readonly CLOSED_SUBMENUS_KEY = 'closedSubmenus';
+  private closedSubmenus = new Set<number>();
 
   items: SidebarItem[] = [];
 
-  constructor(private translationService: TranslationService) { }
+  constructor(
+    private translationService: TranslationService,
+    private router: Router
+  ) {
+    this.routerSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.updateActiveParent());
+  }
 
   ngOnInit() {
+    this.loadClosedSubmenus();
     this.translationService.language$.subscribe(() => {
       this.loadTranslations();
+      this.updateActiveParent();
     });
     this.loadTranslations();
+    this.updateActiveParent();
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription.unsubscribe();
+  }
+
+  private loadClosedSubmenus() {
+    const closed = localStorage.getItem(this.CLOSED_SUBMENUS_KEY);
+    if (closed) {
+      this.closedSubmenus = new Set(JSON.parse(closed));
+    }
+  }
+
+  private saveClosedSubmenus() {
+    localStorage.setItem(this.CLOSED_SUBMENUS_KEY, JSON.stringify(Array.from(this.closedSubmenus)));
   }
 
   toggleSubmenu(index: number): void {
-    this.openedSubmenuIndex = this.openedSubmenuIndex === index ? null : index;
+    if (this.openedSubmenuIndex === index) {
+      this.closedSubmenus.add(index);
+      this.openedSubmenuIndex = null;
+    } else {
+      this.closedSubmenus.delete(index);
+      this.openedSubmenuIndex = index;
+    }
+    this.saveClosedSubmenus();
+  }
+
+  private updateActiveParent() {
+    this.activeParentIndex = null;
+    this.items.forEach((item, index) => {
+      if (item.submenu && this.isSubItemActive(item.itemsSubMenu)) {
+        this.activeParentIndex = index;
+        if (!this.isLeftSidebarCollapsed && !this.closedSubmenus.has(index)) {
+          this.openedSubmenuIndex = index;
+        }
+      }
+    });
+  }
+
+  private isSubItemActive(subItems: SidebarItem[] | undefined): boolean {
+    return subItems?.some(subItem =>
+      subItem.routeLink && this.router.isActive(subItem.routeLink, true)
+    ) || false;
   }
 
   loadTranslations() {
@@ -64,17 +118,17 @@ export class SidebarComponent {
       this.items = [
         {
           routeLink: '/home',
-          icon: 'assets/svg/icon/home.svg',
+          icon: 'assets/svg/icon/sidebar/home.svg',
           label: this.linkHome
         },
         {
-          icon: 'assets/svg/icon/users.svg',
+          icon: 'assets/svg/icon/sidebar/users.svg',
           label: this.linkUsers,
           submenu: true,
           itemsSubMenu: [
             {
               routeLink: '/register',
-              icon: 'assets/svg/icon/register.svg',
+              icon: 'assets/svg/icon/sidebar/register.svg',
               label: this.linkRegister,
             },
             {
@@ -101,7 +155,7 @@ export class SidebarComponent {
               label: "Register"
             },
             {
-              routeLink: '/users',
+              routeLink: '/aaa',
               icon: 'assets/svg/icon/sidebar/listofusers.svg',
               label: "List of storage"
             },
@@ -124,9 +178,9 @@ export class SidebarComponent {
     this.isLeftSidebarCollapsed = !this.isLeftSidebarCollapsed;
     if (this.isLeftSidebarCollapsed) {
       this.openedSubmenuIndex = null;
+    } else {
+      this.updateActiveParent();
     }
     this.changeIsLeftSidebarCollapsed.emit(this.isLeftSidebarCollapsed);
   }
-
-
 }
